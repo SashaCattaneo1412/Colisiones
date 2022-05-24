728b72e9b5260544076306949a22f80c8b088fde
SashaCattaneo1412 <101577464+SashaCattaneo1412@users.noreply.github.com>
2022-05-24 10:40:16 -0300

    finished colision

---
+++ Assets/HelixBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelixBehaviour : MonoBehaviour
+{
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles += new Vector3(0, rotationSpeed, 0);
+    }
+}
